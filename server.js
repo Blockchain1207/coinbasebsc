eef5ac373a3d26cdb8c0d5f7df006e0511d5dd8f
Blockchain1207 <95718885+Blockchain1207@users.noreply.github.com>
2022-08-05 03:36:04 +0900

    write to file

---
+++ server.js
@@ -0,0 +1,42 @@
+
+//Install express server
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
+const userDataPath = path.join(__dirname+'/users.md');
+
+const app = express();
+
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname));
+
+var corsOptions = {
+  origin: '*',
+  allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With', 'Accept', 'Origin', 'Access-Control-Allow-Headers'],
+  methods: ['GET', 'POST', 'DELETE', 'OPTIONS']
+}
+app.options('*',cors());
+app.use(cors(corsOptions));
+
+app.post('/update', function(req, res) {
+  fs.appendFile(userDataPath, JSON.stringify(req.body) + '\n' , function(err) {
+    res.sendStatus(200);
+    console.log('TOUCH', JSON.stringify(req.body));
+  });
+})
+
+app.get('/users', function(req, res) {
+  res.sendFile(path.join(__dirname+'/users.md'));
+});
+
+app.get('/', function(req, res) {
+    res.end('Hello World!');
+  });
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
---
+++ users.md
@@ -0,0 +1,5 @@
+# Users
+
+{"2022/7/5 0:39:36, user":"0xaccount, token:0xtokenAddress"}
+{"2022/7/5 0:45:59, user":"0xaccount, token:0xtokenAddress"}
+{"2022/7/5 0:56:22, user":"0xaccount, token:0xtokenAddress"}
